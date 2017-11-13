198f1d52eb649a1a6ef5e314a38ee6bdbd897a01
Tim Sharp <timothyaaron.sharp@gmail.com>
2017-11-12 21:11:05 -0700

    weather change works, image refresh button put in

--- app/components/weather/weather-controller.js
+++ app/components/weather/weather-controller.js
@@ -4,25 +4,80 @@ function WeatherController() {
 	var weatherElem = document.getElementById('weather-area')
 
 
+	var unit = 'fahrenheit'
+
 
 	//NEED TO GET WEATHER INFO FROM SERVICE TO DRAW ONTO THE SCREEN-----------DONE
 	//NEED TO CONVERT KELVIN TO FAHRENHEIT(STRETCH GOAL MAKE A TOGGLE BETWEEN DEGREE MEASUREMENTS)----------DONE
 
-
+	//LOOK INTO ONCLICK FOR THE WEATHER ID DIV
 
 	function drawWeather() {
+
 		var weatherTemplate = ''
-		weatherService.getWeather(function (weather) {
-			console.log(weather);
-			console.log(weather.main.temp)
-			var fahrenheit = 1.8 * (weather.main.temp - 273) + 32
-			weatherTemplate += `
-			<h2 class="m-tb">${fahrenheit.toFixed(1)}°F <img src="http://openweathermap.org/img/w/${weather.weather[0].icon}.png"><br>
+		if (unit == 'kelvin') {
+			weatherService.getWeatherKelvin(function (weather) {
+				console.log(weather);
+				console.log(weather.main.temp)
+				// var fahrenheit = 9/5 * (weather.main.temp - 273) + 32
+				weatherTemplate += `
+			<h2 class="weather-cycle ilb m-tb" onclick="app.controllers.weatherController.cycleWeather()">${weather.main.temp.toFixed(1)}°K <img src="http://openweathermap.org/img/w/${weather.weather[0].icon}.png"><br>
 			${weather.name}</h2>
+
+			<div class="image-refresh pull-right" onclick="app.controllers.imgController.refreshBackgroundImage()">
+				<i class="fa fa-picture-o" aria-hidden="true"></i><i class="fa fa-refresh" 	aria-hidden="true"></i>
+			</div>
 			`
-			//What can you do with this weather object?
-			weatherElem.innerHTML = weatherTemplate
-		})
+				//What can you do with this weather object?
+				weatherElem.innerHTML = weatherTemplate
+			})
+			unit = 'celsius'
+			return
+		}
+
+		if (unit == 'celsius') {
+			weatherService.getWeatherCelsius(function (weather) {
+				console.log(weather);
+				console.log(weather.main.temp)
+				// var fahrenheit = 9/5 * (weather.main.temp - 273) + 32
+				weatherTemplate += `
+			<h2 class="weather-cycle ilb m-tb" onclick="app.controllers.weatherController.cycleWeather()">${weather.main.temp.toFixed(1)}°C <img src="http://openweathermap.org/img/w/${weather.weather[0].icon}.png"><br>
+			${weather.name}</h2>
+
+			<div class="image-refresh pull-right" onclick="app.controllers.imgController.refreshBackgroundImage()">
+				<i class="fa fa-picture-o" aria-hidden="true"></i><i class="fa fa-refresh" 	aria-hidden="true"></i>
+			</div>
+			`
+				//What can you do with this weather object?
+				weatherElem.innerHTML = weatherTemplate
+			})
+			unit = 'fahrenheit'
+			return
+		}
+
+		if (unit == 'fahrenheit') {
+			weatherService.getWeatherFahrenheit(function (weather) {
+				console.log(weather);
+				console.log(weather.main.temp)
+				// var fahrenheit = 9/5 * (weather.main.temp - 273) + 32
+				weatherTemplate += `
+			<h2 class="weather-cycle ilb m-tb" onclick="app.controllers.weatherController.cycleWeather()">${weather.main.temp.toFixed(1)}°F <img src="http://openweathermap.org/img/w/${weather.weather[0].icon}.png"><br>
+			${weather.name}</h2>
+
+			<div class="image-refresh pull-right" onclick="app.controllers.imgController.refreshBackgroundImage()">
+				<i class="fa fa-picture-o" aria-hidden="true"></i><i class="fa fa-refresh" 	aria-hidden="true"></i>
+			</div>
+			`
+				//What can you do with this weather object?
+				weatherElem.innerHTML = weatherTemplate
+			})
+			unit = 'kelvin'
+
+			return
+		}
+	}
+	this.cycleWeather = function cycleWeather() {
+		drawWeather()
 	}
 	drawWeather()
 }
--- app/components/weather/weather-service.js
+++ app/components/weather/weather-service.js
@@ -1,9 +1,14 @@
 function WeatherService() {
 	var url = '//bcw-getter.herokuapp.com/?url=';
-	var url2 = 'http://api.openweathermap.org/data/2.5/weather?q=boise&&APPID=bd82255fd0a21fa1238699b9eda2ee35'
-	var apiUrl = url + encodeURIComponent(url2);
+	var url2 = 'http://api.openweathermap.org/data/2.5/weather?q=boise'
 
-	this.getWeather = function (weatherDataCb) {
+	var appid = '&&APPID=bd82255fd0a21fa1238699b9eda2ee35'
+	var apiUrl
+	var celsius = '&units=metric'
+	var fahrenheit = '&units=imperial'
+
+	this.getWeatherKelvin = function getWeatherKelvin(weatherDataCb) {
+		apiUrl = url + encodeURIComponent(url2 + appid)
 		$.get(apiUrl, function (res) {
 			res = JSON.parse(res)
 			localStorage.setItem('weather', JSON.stringify(res))
@@ -13,4 +18,28 @@ function WeatherService() {
 			weatherDataCb(res);
 		})
 	}
+	this.getWeatherCelsius = function getWeatherCelsius(weatherDataCb) {
+		apiUrl = url + encodeURIComponent(url2 + celsius + appid)
+		$.get(apiUrl, function (res) {
+			res = JSON.parse(res)
+			localStorage.setItem('weather', JSON.stringify(res))
+			// HEY FUN FACT 
+			// Have you ever wanted to know the temperature measured in kelvin?
+			// You should probably convert the temperature data
+			weatherDataCb(res);
+		})
+	}
+	this.getWeatherFahrenheit = function getWeatherFahrenheit(weatherDataCb) {
+		apiUrl = url + encodeURIComponent(url2 + fahrenheit + appid)
+		$.get(apiUrl, function (res) {
+			res = JSON.parse(res)
+			localStorage.setItem('weather', JSON.stringify(res))
+			// HEY FUN FACT 
+			// Have you ever wanted to know the temperature measured in kelvin?
+			// You should probably convert the temperature data
+			console.log('this is the res', res)
+			weatherDataCb(res);
+		})
+	}
 }
+
